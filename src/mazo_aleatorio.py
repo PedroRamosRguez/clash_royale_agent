bf2478ea94604630494b5a94d5d4ca9b87d62b6a
Pedro Manuel Ramos Rodríguez <pramos@parcan.es>
2018-08-06 09:53:32 +0100

    se envia el mazo creado como objeto para poder calcular coste elixir medio y arena para usar el mazo

--- src/mazo_aleatorio.py
+++ src/mazo_aleatorio.py
@@ -13,11 +13,13 @@ def get_listado_cartas(mazo):
         Método para añadir al objeto del carousel de cards las cards obtenidas
         del mazo aleatorio generado por la API
     """
-    listado_cartas = []
+    listado_cartas,elixirCost,arena = [], [], []
     #la traduccion de la descripcion dejarla pendiente...
     for i in mazo:
         #descripcion = i['description']
         #descripcion_traducida = translator.translate(descripcion, dest='es')
+        elixirCost.append(i['elixirCost'])
+        arena.append(i['arena'])
         listado_cartas.append({
             'info': {
                 'key': i['idName'],
@@ -26,24 +28,25 @@ def get_listado_cartas(mazo):
                 ]
             },
             'title': i['name'],
-            'description': i['description'],#str(descripcion_traducida.text),
+            'description': '',#i['description'],#str(descripcion_traducida.text),
             'image': {
                 'imageUri': 'http://www.clashapi.xyz/images/cards/'+str(i['idName'])+'.png',
                 'accessibilityText': str(i['idName'])
-            }
+            },
         })
-    return listado_cartas
+    return {'listado_cartas':listado_cartas, 'elixirCost':elixirCost, 'arena':arena }
 
 def mazo_aleatorio(request):
     """action de mazo aleatorio
         Action que llama a la API para obtener un mazo aleatorio
     """
-
-    mazo= get_mazo_aleatorio()
+    mazo = get_mazo_aleatorio()
     listado_cartas = get_listado_cartas(mazo)
-    #print(mazo)
     print('ESTO ES EL LISTADO DE CARTAS...')
-    print(listado_cartas)
+    coste_elixir = [i for i in listado_cartas['elixirCost']]
+    arenas = [i for i in listado_cartas['arena']]
+    media_coste_elixir = round(sum(coste_elixir)/ len(coste_elixir))
+    top_arena = max(arenas)
     result = {
         'fulfillmentText': 'este es el mazo resultante',
         'fulfillmentMessages': [
@@ -52,8 +55,8 @@ def mazo_aleatorio(request):
                 'simpleResponses': {
                     'simpleResponses': [
                         {
-                            'textToSpeech': 'este es el mazo resultante',
-                            'displayText': 'Este es el mazo resultante',
+                            'textToSpeech': 'este es el mazo resultante tiene una media de coste de elixir de {} y es un mazo de arena {}'.format(media_coste_elixir,top_arena),
+                            'displayText': 'Mazo resultante.\n Media de coste de elixir de {} .\n Mazo de arena {}'.format(media_coste_elixir,top_arena),
                         }
                     ]
                 }
@@ -61,7 +64,7 @@ def mazo_aleatorio(request):
             {
                 'platform': 'ACTIONS_ON_GOOGLE',
                 'carouselSelect': {
-                    'items': [i for i in listado_cartas]
+                    'items': [i for i in listado_cartas['listado_cartas']]
                 },
             },
         ],

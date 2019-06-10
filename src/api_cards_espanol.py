87e02dc412bf6be758844d73bb7c08997f7275cd
Pedro Ramos <p.ramos1990@gmail.com>
2019-06-10 12:47:35 +0100

    Update api_cards_espanol.py
    Eliminado caracter ñ

--- src/api_cards_espanol.py
+++ src/api_cards_espanol.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify, abort, make_response, Blueprint
 import cards
 
-api_card_español = Blueprint('api_card_español', __name__)
-@api_card_español.route('/status/api/',methods=['GET'])
+api_card_español = Blueprint('api_card_espaniol', __name__)
+@api_card_espaniol.route('/status/api/',methods=['GET'])
 def status_api():
     return 'Status API OK'
 
-@api_card_español.route('/api/card/', methods=['GET'])
+@api_card_espaniol.route('/api/card/', methods=['GET'])
 def get_cards():
     """
         Función para obtener toda la información de las cartas de la API
     """
     return jsonify(cards.cards)
 
-@api_card_español.route('/api/card/<string:idName>', methods=['GET'])
+@api_card_espaniol.route('/api/card/<string:idName>', methods=['GET'])
 def get_card(idName):
     """
         Función para obtener carta específica en la API
@@ -24,7 +24,7 @@ def get_card(idName):
         abort(404)
     return jsonify(card[0])
 
-@api_card_español.errorhandler(404)
+@api_card_espaniol.errorhandler(404)
 def not_found(error):
     """ Función para manejar el error en caso de no encontrar la carta en la API"""
     return make_response(jsonify({'error': 'Carta no encontrada'}), 404)
